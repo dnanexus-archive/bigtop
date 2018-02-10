bea09c06b785652d3f3b569c77cd0794dce33e7c
Chris Meyer <cmeyer@dnanexus.com>
2018-02-09 16:14:25 -0800

    Breaking sample object into scene

--- index.vr.js
+++ index.vr.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {AppRegistry} from 'react-vr';
 import World from './vr/components/worlds/manhattan';
+import Pedestal from './vr/components/scenes/pedestal';
 
 export default class ManhattanProject extends React.Component {
   render() {
-    return <World />;
+    return (
+      <World>
+        <Pedestal></Pedestal>
+      </World>
+    );
   }
 };
 
--- vr/components/worlds/manhattan/index.js
+++ vr/components/worlds/manhattan/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import {asset, Cylinder, Sphere, Text, View, PointLight} from 'react-vr';
+import {asset, View, PointLight} from 'react-vr';
 
 export default class Manhattan extends React.Component {
   render() {
     return (
       <View>
-        <Cylinder
-          radiusTop={30}
-          radiusBottom={30}
-          dimHeight={50}
-          segments={24}
-          lit={true}
-          style={{transform: [{translate: [0, -50, -150]}]}}
-        ></Cylinder>
         <PointLight
           decay={2}
           style={{
@@ -27,14 +19,7 @@ export default class Manhattan extends React.Component {
             transform: [{translate: [50, 100, 0]}]
           }}
         ></PointLight>
-        <Sphere
-          radius={20}
-          lit={true}
-          style={{
-            color: "#9970d8",
-            transform: [{translate: [0, -5, -150]}]
-          }}
-        ></Sphere>
+        {this.props.children}
       </View>
     )
   }
