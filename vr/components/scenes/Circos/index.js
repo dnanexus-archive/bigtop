4d3654b05476c7994b47c7724b0cf481616ffe6b
Sam Westreich <swestreich@dnanexus.com>
2018-04-06 15:42:31 -0700

    deleted:    data/10k_GIANT_height.located.coords.json 	deleted:    data/1k_Chr10_GIANT_height.located.coords.json 	deleted:    data/GIANT_height_25k.located.coords.json 	deleted:    data/GIANT_random250.located.coords.json 	deleted:    data/GIANT_random250.located.txt 	modified:   vr/components/scenes/Circos/index.js

--- vr/components/scenes/Circos/index.js
+++ vr/components/scenes/Circos/index.js
@@ -4,7 +4,7 @@ import {createChromosomeScale} from '../../../utils';
 import Floor from '../../molecules/Floor';
 import Rotunda from '../../molecules/Rotunda';
 import PointCloud from '../../molecules/PointCloud';
-import dataPoints from '../../../../data/GIANT_random250.located.coords.json';
+import dataPoints from '../../../../data/1k_GIANT_height.gene_loc.coords.json';
 
 export default class Circos extends React.Component {
   constructor(props) {
