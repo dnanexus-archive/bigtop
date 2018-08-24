61e4984a9b22862051ccc4848a898e41bf2f36b3
Chris Meyer <cmeyer@dnanexus.com>
2018-08-24 14:25:12 -0700

    Fixing SelectedPoint compilation error

--- src/components/molecules/SelectedPoint/index.js
+++ src/components/molecules/SelectedPoint/index.js
@@ -23,13 +23,13 @@ class SelectedPoint extends Component {
     const size = sizeScaler(datum.coords[1]);
 
     let pointDistance = Math.cbrt(Math.pow(Math.abs(datum.coords[0]), 3) + Math.pow(Math.abs(datum.coords[2]), 3) + Math.pow(Math.abs(datum.coords[1]), 3))
-    var r = (datum.radius + radius) / 2
+    let r = (datum.radius + radius) / 2
     let {x, z} = polarToCartesian(r, datum.theta);
 
-		// switching infopanel location based on height
-		if (datum.coords[1] < 4) {var infoHeight = pointDistance / 7 ;
-		} else {
-			var infoHeight = -pointDistance / 7 ;
+    // switching infopanel location based on height
+    let infoHeight = pointDistance / 7;
+		if (datum.coords[1] >= 4) {
+      infoHeight = -infoHeight;
 		}
 
 
@@ -49,19 +49,11 @@ class SelectedPoint extends Component {
           position={{x: x, y: 0, z: z}}
           rotation={{x:90, y: -((datum.theta*180) / Math.PI), z: 0}}
         />
-<<<<<<< HEAD:src/components/atoms/SelectedPoint/index.js
-        <InfoPanel
+        <GeneInfoPanel
           position={{x: 0, y: infoHeight, z: 0}}
           scale={{x: pointDistance, y: pointDistance, z: pointDistance}}
           rotation={{x: 45, y: datum.theta * -57.2958, z: 0}}
-          text={[datum.id, datum.gene, `p: ${datum.p}`, `freq: ${datum.frequency}`, `${datum.chr}:${datum.location}`].join('\n')}
-=======
-        <GeneInfoPanel
-          position={{x: 0, y: (pointDistance / 7), z: 0}}
-          scale={{x: pointDistance, y: pointDistance, z: pointDistance}}
-          rotation={{x: 0, y: datum.theta * -57.2958, z: 0}}
           data={datum /* Only a little weird to turn a datum into data, but the datum does contain many data.... */}
->>>>>>> b29912a5a1ac552590cf8b728e39563d918f5b90:src/components/molecules/SelectedPoint/index.js
         />
 
     </Entity>
