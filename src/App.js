2406456196b97d8347ab4fdd3ba4f75c86e13031
Chris Meyer <cmeyer@dnanexus.com>
2018-09-07 16:21:19 -0700

    Changing p-value cutoff to 1e-7

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
   render() {
     const roomRadius = 10; // meters
     const roomHeight = 10; // meters
-    const pCutoff = 5e-7;
+    const pCutoff = 1e-7;
 
     const chroms = ["chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22", "chrX", "chrY"];
     const sizes = [248956422, 242193529, 198295559, 190214555, 181538259, 170805979, 159345973, 145138636, 138394717, 133797422, 135086622, 133275309, 114364328, 107043718, 101991189, 90338345, 83257441, 80373285, 58617616, 64444167, 46709983, 50818468, 156040895, 57227415];
