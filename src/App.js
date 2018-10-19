e7e5684a94acd5a330f6c9a4d7243639ab7e3990
Sam Westreich <swestreich@dnanexus.com>
2018-10-19 16:47:52 -0700

    glowing points but with lag and errors

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {Entity, Scene} from "aframe-react";
 import React, {Component} from "react";
 import {Provider} from "react-redux";
 import queryString from 'query-string';
-import * as R from 'ramda';
 import PointPlane from "components/atoms/PointPlane";
 import PointCloud from "components/molecules/PointCloud";
 import HandControls from "components/molecules/HandControls";
@@ -12,13 +11,13 @@ import Rotunda from "components/complexes/Rotunda";
 import Floor from "components/complexes/Floor";
 
 // Breast cancer, no significant points :(
-// import sampleData from "data/100k_breast_cancer.coords.json";
+// import data from "data/100k_breast_cancer.coords.json";
 
 // Height:
-import sampleData from "data/90k_GIANT_height_filtered.gene_loc.coords.json";
-
+import data from "data/90k_GIANT_height_filtered.gene_loc.coords.json";
 import cytobands from "data/human_genome_cytoband_edges.json";
 import {createChromosomeScale, calculateCoordinates} from "utils";
+import * as R from 'ramda';
 import configureStore from "./store/configureStore";
 import initialState from "./store/initialState";
 
@@ -27,14 +26,7 @@ const queryParams = queryString.parse(window.location.search);
 const store = configureStore(R.mergeDeepLeft({
   user: {
     rightHanded: typeof queryParams.lefty === 'undefined'
-  },
-  room: {
-    height: (queryParams['room.height'] && parseInt(queryParams['room.height'], 10)) || initialState.room.height,
-    radius: (queryParams['room.radius'] && parseInt(queryParams['room.radius'], 10)) || initialState.room.radius,
-  },
-  pCutoff: (queryParams.p && parseFloat(queryParams.p, 10)) || initialState.pCutoff,
-  pointCount: (queryParams.points && parseInt(queryParams.points, 10)) || initialState.pointCount,
-  data: sampleData // TODO: When we load data dynamically, this will become an async event
+  }
 }, initialState));
 
 class App extends Component {
@@ -49,33 +41,31 @@ class App extends Component {
   componentDidMount() {
     const scene = document.querySelector("a-scene");
     scene.addEventListener("enter-vr", () => {
-      this.setState({inVR: true});
+      this.setState({ inVR: true });
     });
     scene.addEventListener("exit-vr", () => {
-      this.setState({inVR: false});
+      this.setState({ inVR: false });
     });
   }
 
   render() {
-    const state = store.getState();
-    const roomHeight = state.room.height;
-    const roomRadius = state.room.radius;
+    const roomRadius = 10; // meters
+    const roomHeight = 10; // meters
+    const pCutoff = 1e-7;
 
-    const chroms = R.pluck("label", state.chromosomes);
-    const sizes = R.pluck("size", state.chromosomes);
-    const colorScheme = R.pluck("color", state.chromosomes);
+    const chroms = ["chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22", "chrX", "chrY"];
+    const sizes = [248956422, 242193529, 198295559, 190214555, 181538259, 170805979, 159345973, 145138636, 138394717, 133797422, 135086622, 133275309, 114364328, 107043718, 101991189, 90338345, 83257441, 80373285, 58617616, 64444167, 46709983, 50818468, 156040895, 57227415];
     const chromDict = createChromosomeScale(chroms, sizes);
+    const colorScheme = ["#E41A1C", "#A73C52", "#6B5F88", "#3780B3", "#3F918C", "#47A266", "#53A651", "#6D8470", "#87638F", "#A5548D", "#C96555", "#ED761C", "#FF9508", "#FFC11A", "#FFEE2C", "#EBDA30", "#CC9F2C", "#AD6428", "#BB614F", "#D77083", "#F37FB8", "#DA88B3", "#B990A6", "#999999"];
 
     // Use full dataset
-    let downsampledData = state.data;
+    let downsampledData = data;
 
     // Choose the subset with the highest p-values
-    if (state.pointCount) {
-      downsampledData = R.compose(
-        R.slice(0, state.pointCount),
-        R.sortBy(R.prop("p"))
-      )(downsampledData);
-    }
+    // let downsampledData = R.compose(
+    //    R.slice(0, 5000),
+    //    R.sortBy(R.prop("p"))
+    // )(data);
 
     let {coordinates, yScaleDomain, radiusScaleInfo} = calculateCoordinates(
       downsampledData,
@@ -84,7 +74,7 @@ class App extends Component {
       roomHeight
     );
 
-    const {sigCoords = [], insigCoords = []} = R.groupBy((coord) => {return coord.p < state.pCutoff ? 'sigCoords' : 'insigCoords'}, coordinates)
+    const {sigCoords = [], insigCoords = []} = R.groupBy((coord) => {return coord.p < pCutoff ? 'sigCoords' : 'insigCoords'}, coordinates)
 
     const sceneOpts = {
       style: "position: absolute; height: 100%; width: 100%",
