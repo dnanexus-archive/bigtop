32d5392d7fc2aa610f7e0f784f800f26d5ad4c93
Sam Westreich <transcript@users.noreply.github.com>
2019-12-13 13:59:11 -0800

    Cleaning up Python code

--- scripts/rsID_to_loc.py
+++ scripts/rsID_to_loc.py
@@ -1,7 +1,19 @@
 #!/usr/env Python
 
 # rsID_to_loc.py
-# This (hopefully short) Python program takes a file with SNP IDs (rs######)
+# This Python program takes a file with SNP IDs (rs######) and produces the location, retrieved from 
+# the UCSC Genome Browser using CruzDB.  For more info on CruzDB, see here: https://github.com/brentp/cruzdb
+#
+# Input structure: This takes a tab-separated values list of rsIDs (SNP IDs), such as:
+# rs1113396	T	C	0.058	-0.014	0.0063	0.026	230497
+#
+# Note that the SNP ID should be the first column.  The column containing the p-value can be set below this line.
+#
+###### CUSTOMIZABLE: COLUMN CONTAINING P-VALUE FOR EACH SNP ######
+#
+pval_col = 6
+#
+###### END CUSTOMIZATION ######
 
 import cruzdb, sys
 
@@ -22,7 +34,7 @@ for line in infile:
 	line_count += 1
 	splitline = line.split("\t")
 	if splitline[0][:2] == "rs":
-		if float(splitline[6].strip()) > 0.95:
+		if float(splitline[pval_col].strip()) > 0.95:
 			continue
 		else:
 			processed_count += 1
@@ -30,12 +42,11 @@ for line in infile:
 			split_info = str(info).split("\t")
 			# writing to outfile
 			try:
-				outfile.write("\t".join([splitline[0], split_info[0], split_info[1], splitline[1], splitline[2], splitline[3], splitline[6]]) + "\n")
+				outfile.write("\t".join([splitline[0], split_info[0], split_info[1], splitline[1], splitline[2], splitline[3], splitline[pval_col]]) + "\n")
 			except IndexError:
 				print line
 				if str(info) == 'None':
 					error_count += 1
-#					outfile.write("\t".join([splitline[0], "No_info", "N/A", splitline[1], splitline[2], splitline[3], splitline[6]]) + "\n")
 			if processed_count % 100 == 0:
 				print "\t".join([str(line_count), str(processed_count), str(error_count), str(info)])
 	else:
